13a4cb1fc0ed405780561d6a85dc9b0df8584108
matrixbirds <yooobuntu@163.com>
2018-12-28 23:46:53 +0800

    fix. remove unused

--- examples/cxx/main.cpp
+++
@@ -1,9 +0,0 @@
-# include <iostream>
-#include <websocketpp/config/asio_no_tls_client.hpp>
-#include <websocketpp/client.hpp>
-
-using namespace std;
-int main(int argc, char** argv) {
-    cout << " String " << endl;
-    return 0;
-}
